029bd969cfc25db74951d06cbe23a288146b98ca
ajagupta <akumar298@gmail.com>
2019-06-20 16:14:35 +0100

    enabled route base sample program

--- src/App.js
+++ src/App.js
@@ -1,46 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Counter from './components/Counter'
-import TextNoteBook from './components/TextNoteBook'
-import NoteList from './components/NoteList'
+
+
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import {postNotesReducer,userOptReducer} from './reducers/index'
+import { postNotesReducer, userOptReducer } from './reducers/index'
 import counterReducer from './reducers/counterReducer'
 import { composeWithDevTools } from 'redux-devtools-extension';
-import TableContainer from './containers/TableContainer'
+
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter } from 'react-router-dom';
+
+import { Header } from './components/header';
+import RootRouter from './components/route';
 
 const reducer = combineReducers({
   counterReducer: counterReducer,
   posts: postNotesReducer,
-  users:userOptReducer
+  users: userOptReducer
 });
+
+
 
 const store = createStore(reducer, composeWithDevTools());
 function App() {
   return (
     <div className="container">
+      
       <Provider store={store}>
-        <div className="row">
-          <div className="col">
-            <Counter />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <TableContainer />
-          </div>
-        </div>
-        <div className="row">
-          <div className="col">
-            <TextNoteBook />
-          </div>
-          <div className="col">
-            <NoteList />
-          </div>
-        </div>
+      <BrowserRouter>
+        <Header/>
+        <RootRouter/>
+        </BrowserRouter>
       </Provider>
     </div>
   );
